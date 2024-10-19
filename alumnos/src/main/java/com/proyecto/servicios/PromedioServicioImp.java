7ebd79d8287fc86f9caeadf1d24be28f35e0618c
ELIO DURAN <elioduran60@gmail.com>
2024-10-19 18:58:56 -0300

    Actualizacion de la clase PromedioServicioImp

--- alumnos/src/main/java/com/proyecto/servicios/PromedioServicioImp.java
+++ alumnos/src/main/java/com/proyecto/servicios/PromedioServicioImp.java
@@ -1,5 +1,16 @@
 package com.proyecto.servicios;
 
+import java.util.List;
+
+
 public class PromedioServicioImp {
 
+    public double calcularPromedio(List<Double> notas) {//Ingresan notas para llenar Lista de Notas
+        if (notas == null || notas.isEmpty()) { // Valida que el ingreso de notas no tenga valor nulo ni que esté vacia esa informacion. 
+            return 0.0;
+        } else {
+            return notas.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);//Calcula el promedio con la clase "stream" y el 
+            																		//método "average", en caso de fallo  tendrá salida de 0.0
+        }
+    }
 }
