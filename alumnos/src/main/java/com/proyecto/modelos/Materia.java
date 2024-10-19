aa6ccb98079902c789267204d93d1bfe9e6462a9
ELIO DURAN <elioduran60@gmail.com>
2024-10-19 18:49:21 -0300

    Actualizacion de la clase Materia

--- alumnos/src/main/java/com/proyecto/modelos/Materia.java
+++ alumnos/src/main/java/com/proyecto/modelos/Materia.java
@@ -1,5 +1,40 @@
 package com.proyecto.modelos;
 
-public class Materia {
+import java.util.List;
+import java.util.ArrayList;
 
+import com.proyecto.interfaces.IScanner;
+
+public class Materia implements IScanner {
+    private MateriaEnum nombremateria;//Vairable del tipo Enum
+    private List<Double> notas;
+
+    public Materia(int opcion) {
+        notas = new ArrayList<>();
+        switch (opcion) { // Selector de las materias disponibles.
+            case 1 -> this.nombremateria = MateriaEnum.MATEMATICAS;
+            case 2 -> this.nombremateria = MateriaEnum.LENGUAJE;
+            case 3 -> this.nombremateria = MateriaEnum.CIENCIA;
+            case 4 -> this.nombremateria = MateriaEnum.HISTORIA;
+            default -> throw new IllegalArgumentException("Opción inválida para materia");
+        }
+
+    }
+
+    //Metódos para las Materias y notas:
+    public MateriaEnum getNombremateria() {
+        return nombremateria;
+    }
+
+    public void setNombremateria(MateriaEnum nombremateria) {
+        this.nombremateria = nombremateria;
+    }
+
+    public List<Double> getNotas() {
+        return notas;
+    }
+
+    public void setNotas(List<Double> notas) {
+        this.notas = notas;
+    }
 }
